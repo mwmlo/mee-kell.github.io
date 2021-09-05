7c8090467273a8ba81931ee66018397108d35dba
Michelle Lo <lomichelle42@gmail.com>
2021-09-05 10:53:25 +0800

    fix: :bug: Fix link redirects onto new tabs

--- src/components/Minicard.js
+++ src/components/Minicard.js
@@ -4,7 +4,8 @@ import '../styles/components.css';
 
 export default function Minicard(props) {
     return (
-        <a href={props.url} className="minicard" data-aos="zoom-in">
+        <a href={props.url} target="_blank" rel="noreferrer" 
+            className="minicard" data-aos="zoom-in">
             <h3>{props.title}</h3>
             <p>{props.desc}</p>
         </a>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
         <div className="contact">
             <h1 data-aos="fade">Get in touch</h1>
             <p>
-                You can check out my <a href="https://github.com/mee-kell/" className="contact-btn">👩‍💻 Github</a> for more information, or email me at <a href="michelle.lo21@imperial.ac.uk" className="contact-btn">📬 michelle.lo21@imperial.ac.uk</a>.
+                You can check out my <a href="https://github.com/mee-kell/" target="_blank" rel="noreferrer" className="contact-btn">👩‍💻 Github</a> for more information, or email me at <a href="michelle.lo21@imperial.ac.uk" target="_blank" rel="noreferrer" className="contact-btn">📬 michelle.lo21@imperial.ac.uk</a>.
             </p>
             <div className="footer">
                 Copyright © 2021 Michelle Wai Man Lo
